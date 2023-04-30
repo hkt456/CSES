01b3867fb9862cad66e773bebf629cb9bd5e59dc
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-04-30 17:46:35 +0700

    update

--- Dynamic Programming/minimizing-coins.cpp
+++ Dynamic Programming/minimizing-coins.cpp
@@ -6,18 +6,18 @@ using namespace std;
 int main(){
     int n, x;
     cin>>n>>x;
-    int coins[n], dp[x+5];
-    memset(dp, maxC, sizeof(dp));
+    long long coins[n], dp[x+5];
+	for (int i = 0; i <= x+5; i++) { dp[i] = INT_MAX; }
     for(int i=0; i<n; i++){
         cin>>coins[i];
     }
     dp[0]=0;
     for(int i=0; i<n; i++){
-        for(int j=0; j<=x; j++){
+        for(int j=1; j<=x; j++){
             if(j-coins[i]>=0){
                 dp[j]=min(dp[j],dp[j-coins[i]]+1);
             }
         }
     }
-    cout<<(dp[x]==maxN?-1:dp[x]);
+    cout<<(dp[x]==INT_MAX?-1:dp[x]);
 }
