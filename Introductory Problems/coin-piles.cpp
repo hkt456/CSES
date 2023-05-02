459692b0a24d1058ae2fdd556e158dc5f7a23c1d
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-05-02 11:58:56 +0700

    first program of the day

--- Introductory Problems/coin-piles.cpp
+++ Introductory Problems/coin-piles.cpp
@@ -4,3 +4,23 @@ using namespace std;
 typedef long long ll;
 
 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        if(a<b)swap(a,b);
+        if(a>2*b){
+            cout<<"NO\n";
+        }else{
+            a%=3,b%=3;
+            if(a<b)swap(a,b);
+            if((a==2 && b==1) || (a==b && b==0)){
+                cout<<"YES\n";
+            }else{
+                cout<<"NO\n";
+            }
+        }
+    }
+}
