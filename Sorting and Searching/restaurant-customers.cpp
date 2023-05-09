0dc7ffb7cf6a0dd0a5b50944ff9febd55b38c3ee
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-05-09 15:57:39 +0700

    sort prob 1

---
+++ Sorting and Searching/restaurant-customers.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<pair<int,int>> times;
+    for(int i=0; i<n; i++){
+        int start, end;
+        cin>>start>>end;
+        times.push_back({start, 1});
+        times.push_back({end, -1});
+    }
+    sort(times.begin(), times.end());
+    int current=0, maxppl=0;
+    for(const pair<int,int> t : times){
+        current+=t.second;
+        maxppl=max(maxppl, current);
+    }
+    cout<<maxppl;
+}
